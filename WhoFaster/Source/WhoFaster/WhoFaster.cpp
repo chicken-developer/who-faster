340df32f55469abdb3778f3d2d8c3f740db84ac8
Nguyen Manh Quynh <quynh.fullstackdev@gmail.com>
2022-06-16 04:32:55 +0700

    Remove client

--- WhoFaster/Source/WhoFaster/WhoFaster.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "WhoFaster.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, WhoFaster, "WhoFaster" );
